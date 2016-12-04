89ab4ab8655e16463f13c5f48108f0d1b3fe64df
Elvin Punsalan <elvinpunsalan@gmail.com>
2016-12-04 12:03:18 -0600

    Contact page

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+Contact.delete_all
+Contact.create!(name: 'Example Name', email: 'contact@example.com', phone: '(555)555-5555', notes: 'Enter some notes here.')
